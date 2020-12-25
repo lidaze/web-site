a4a55526caf38cdb44ac1512f368f00916e73d8a
Joyful <lidaze@users.noreply.github.com>
2020-12-25 16:21:04 +0800

    refactor: change style

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div style={{ fontSize: 350, color: '#f0f0f0', letterSpcing: '20px', fontWeight: 'bold' }}>JOYFUL</div>
+      <div
+        style={{
+          fontSize: 50,
+          color: "#fafafa",
+          letterSpacing: "20px",
+          fontWeight: "bold"
+        }}
+      >
+        JOYFUL
+      </div>
     </div>
-  )
+  );
 }
