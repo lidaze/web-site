8d21eabd67c584bda74bcd4f849c439a64e709a7
Joyful <lidaze@users.noreply.github.com>
2020-12-29 20:33:27 +0800

    refactor: detail page refactor

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from "react";
 import styles from "../../styles/Blog.module.scss";
 interface BlogProps {
-  slug: string;
+  slug?: string;
+  data: { title: string; body: string };
 }
 
-const Blog: React.FC<BlogProps> = ({ slug }) => {
-  const [data, setData] = useState(null);
+const Blog: React.FC<BlogProps> = ({ data }) => {
+  // const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
 
   console.count("Blog");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      const res = await fetch(`/api/detail?slug=${slug}`);
-      const data = await res.json();
-      console.log(data);
-      if (data.status === 200) {
-        setData(data.data);
-      }
-      setLoading(false);
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setLoading(true);
+  //     const res = await fetch(`/api/detail?slug=${slug}`);
+  //     const data = await res.json();
+  //     console.log(data);
+  //     if (data.status === 200) {
+  //       setData(data.data);
+  //     }
+  //     setLoading(false);
+  //   };
 
-    fetchData();
-  }, [slug]);
+  //   fetchData();
+  // }, [slug]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -42,16 +43,44 @@ const Blog: React.FC<BlogProps> = ({ slug }) => {
 };
 
 export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const userKey = "joyful";
+  const postKey = "tdnx2e";
+  const token = "aPLzrTRapohImKwomnZ30FDuZX1KGoMeA6fSZwF7";
+
+  const domainStr = "https://www.yuque.com/api/v2";
+  const url = `${domainStr}/repos/${userKey}/${postKey}/docs/${slug}`;
+  const res = await fetch(url, {
+    headers: {
+      "X-Auth-Token": token
+    }
+  });
+  const resJson = await res.json();
+
   return {
     props: {
-      slug: params.slug
+      ...resJson
     }
   };
 };
 
 export const getStaticPaths = async () => {
+  const userKey = "joyful";
+  const postKey = "tdnx2e";
+  const token = "aPLzrTRapohImKwomnZ30FDuZX1KGoMeA6fSZwF7";
+
+  const domainStr = "https://www.yuque.com/api/v2";
+  const url = `${domainStr}/repos/${userKey}/${postKey}/docs`;
+  const res = await fetch(url, {
+    headers: {
+      "X-Auth-Token": token
+    }
+  });
+  const data = await res.json();
+  const slugs = data.data.map((item) => item.slug);
+
   return {
-    paths: [2].map((item) => `/blog/${item}`),
+    paths: slugs.map((item) => `/blog/${item}`),
     fallback: true
   };
 };
